24b0b051c1deb610ebf89ddf43785d0d655a88e5
Youssef <youssef.auassar@student.ehb.be>
2024-10-03 15:53:04 +0200

    Styles: Add styling for ColorPicker and App components

--- challenge-1/src/App.jsx
+++ challenge-1/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
-import "./App.css";
+import React from 'react';
+import HomePage from './pages/HomePage';
+import './styles/App.css';
 
-function App() {
-	return <></>;
-}
+// The App component acts as the root component of the application
+const App = () => {
+  return (
+    <div className="app-container">
+      <HomePage />
+    </div>
+  );
+};
 
 export default App;
